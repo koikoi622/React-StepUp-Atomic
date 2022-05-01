7275f37d082d345eb895f2f10b3dec1b9ad4eee6
koikoi622 <104248045+koikoi622@users.noreply.github.com>
2022-05-01 22:52:22 +0900

    05012252

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,6 @@
-import { BrowserRouter } from "react-router-dom";
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organisms/user/UserCard";
-import { DefaultLayout } from "./components/templates/DefaultLayout";
-
+import { Router } from "./router/Router";
 import "./styles.css";
 
-const user = {
-  name: "てすと名前",
-  image: "https://source.unsplash.com/NE0XGVKTmcA",
-  email: "12345@example.com",
-  phone: "000-1111-2222",
-  company: {
-    name: "テスト株式会社"
-  },
-  website: "https://google.com"
-};
-
 export const App = () => {
-  return (
-    <BrowserRouter>
-      <DefaultLayout>
-        <PrimaryButton>テスト</PrimaryButton>
-        <SecondaryButton>検索</SecondaryButton>
-        <br />
-        <SearchInput />
-        <UserCard user={user} />
-      </DefaultLayout>
-    </BrowserRouter>
-  );
+  return <Router />;
 };
